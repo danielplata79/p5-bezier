2283245555a3c20c668242183982880be737ef4b
Daniel Plata <danielplata79@gmail.com>
2024-03-25 03:45:41 -0500

    orbitControl()

--- orbit.js
+++ orbit.js
@@ -1,12 +1,45 @@
+// Docs at https://www.geeksforgeeks.org/p5-js-orbitcontrol-function/
+
+let newFont;
+let orbitControlEnable = false;
+
+function preload() {
+    newFont = loadFont('assets/JetBrains Mono Regular Nerd Font Complete Mono.ttf')
+}
+
 function setup() {
-    createCanvas(500,500)
-    slider = createSlider(0, 255)
-    slider.position(50,50)
-    slider.size(100)
+    createCanvas(600,600,WEBGL)
+    textFont(newFont, 18)
+
+    xSensivitySlider = createSlider(0, 5, 1, 0.1)
+    xSensivitySlider.position(20, 50)
+
+    ySensivitySlider = createSlider(0, 5, 1, 0.1)
+    ySensivitySlider.position(20,100)
+
+    zSensivitySlider = createSlider(0, 5, 1, 0.1)
+    zSensivitySlider.position(20, 150)
+    
+    // camera(0, 0, 50*sqrt(3), 0, 0, 0, 0, 1, 0);
+    // perspective(PI/3, 1, 5*sqrt(3), 500*sqrt(3));
 }
 
 function draw() {
-    let c = slider.value()    
+    background(0)
 
-    background(c)
+    // rotateX(frameCount * 0.01);
+    // rotateY(frameCount * 0.01);
+
+    fill(255)
+    textSize(30)
+    text("xSensivitySldier value: " + xSensivitySlider.value(), 0, 300)
+
+
+    xSensivity = xSensivitySlider.value()
+    ySensivity = ySensivitySlider.value()
+    zSensivity = zSensivitySlider.value()
+
+    orbitControl(xSensivity,ySensivity,zSensivity)
+ 
+    box(50)
 }

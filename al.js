fc375b4667408af5cb58d57f918d2d23a3d72123
Daniel Plata <danielplata79@gmail.com>
2024-04-02 04:09:05 -0500

    ambientLight()2

---
+++ al.js
@@ -0,0 +1,16 @@
+let newFont;
+let currentLightColor = "red";
+let currentAmbientColor = "red";
+
+function preload() {
+    newFont = loadFont('assets/JetBrains Mono Regular Nerd Font Complete Mono.ttf')
+}
+
+function setup() {
+	createCanvas(600,600,WEBGL)
+}
+
+function draw() {
+	background(0)
+	let locX = mouseX	
+}

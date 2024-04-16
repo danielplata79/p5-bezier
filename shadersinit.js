21568521bb2ab434666b05eb925547d0dfbb97ca
Daniel Plata <danielplata79@gmail.com>
2024-04-16 16:28:18 -0500

    shader loading & shader()

---
+++ shadersinit.js
@@ -0,0 +1,15 @@
+let theShader;
+
+function preload(
+	theShader = loadShader('shader.vert',)
+)
+
+function setup(
+	createCanvas(windowWidth,windowHeight,WEBGL)
+	noStroke()
+)
+
+function draw(
+	shader(theShader)
+	rect(0,0,width,height)
+)

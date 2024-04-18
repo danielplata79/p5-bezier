9e5aa0f502fe529252178574b7672145b249d760
Daniel Plata <danielplata79@gmail.com>
2024-04-18 04:41:41 -0500

    noise() graph and how Sin works the same way

--- noisegraphsin.js
+++ noisegraphsin.js
@@ -9,15 +9,29 @@ function draw() {
 	background(0)
 	noFill()
 
+	// Using random() instead of noise()
+	//
+	//beginShape()
+	//for( var x = 0; x < width; x++ ) {
+	//	stroke(255)
+	//	vertex(x, random(height))
+	//}
+	//endShape()
+	
 	beginShape()
 	var xoff = start
 	for(var x = 0; x < width; x++) {
 		stroke(255)
+		//var y = random(height)
 		var y = noise(xoff) * height
+		//var y = sin(xoff) * height
 		vertex(x,y)
+
 		xoff += inc
 	}
 	endShape()
 	
 	start += inc
+	
+	//noLoop()
 }

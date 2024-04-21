16788d79069d4742b3fd1519fc62cb3630625596
Daniel Plata <danielplata79@gmail.com>
2024-04-21 17:10:12 -0500

    cloud noise with noise()

--- cloud-noise.js
+++ cloud-noise.js
@@ -1,21 +1,29 @@
 var inc = 0.01;
 
 function setup() {
-	createCanvas(600, 600);
+	createCanvas(920, 290);
 	pixelDensity(1);
 }
 
 function draw() {
+	var yoff = 0;
+
 	loadPixels();
 	for (var x = 0; x < width; x++) {
+		var xoff = 0;
 		for (var y = 0; y < height; y++ ) {
 			var index = (x + y * width) * 4;
-			var r = random(255);
+			var r = noise(xoff,yoff) * 255;
 			pixels[index + 0] = r;
 			pixels[index + 1] = r;
 			pixels[index + 2] = r;
 			pixels[index + 3] = 255;
+			
+			//xoff += inc;
+			xoff += inc;
 		}
+		yoff += inc;
 	}
 	updatePixels();
+	//noLoop();
 }

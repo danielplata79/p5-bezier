c4933c6b6dccab542aef91dc37927629a84b744f
Daniel Plata <danielplata79@gmail.com>
2024-04-25 18:48:14 -0500

    bezier initalized

---
+++ bezier.js
@@ -0,0 +1,16 @@
+let n;
+let offset = 0;
+let x1; let y1;
+let x2; let y2;
+let x3; let y3;
+let x4; let y4;
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+		
+
+}

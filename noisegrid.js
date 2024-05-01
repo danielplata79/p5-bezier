81374531d6a943890a2b48ad353357e34aa15a09
Daniel Plata <danielplata79@gmail.com>
2024-04-30 23:05:53 -0500

    noisegrid and grids 2

--- noisegrid.js
+++ noisegrid.js
@@ -1,6 +1,22 @@
-let size = 50; let cols, let rows;
+let cols; let rows; let size = 100;
 let xoff = 0; let yoff = 0;
 let arrows = [];
 
-function setup() {}
-function draw() {}
+let inc = 0.01;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	cols = height/size;
+	rows = width/size;
+}
+
+function draw() {
+	background(0);
+
+	for(let x=0; x<rows; x++) {
+		arrows[x] = [];
+		for(let y=0; y<cols; y++) {
+		rect(x*size,y*size, size, size);
+		}
+	}
+}

485b8223d0b955441386f5af899b0fc85b1c9c44
Daniel Plata <danielplata79@gmail.com>
2024-05-07 19:18:38 -0500

    Tommy04052024

---
+++ noiseterrain.js
@@ -0,0 +1,13 @@
+// En Memoria de Tommy Andres Plata Garcia
+// 9 de Noviembre del 2006 - 4 de Mayo del 2024
+// Buen viaje.
+//
+// Re-creado por Daniel Plata
+// Con ayuda de The Code Train
+
+function setup() {
+	createCanvas(windowWidth, windowHeight, WEBGL);
+}
+function draw() {
+
+}

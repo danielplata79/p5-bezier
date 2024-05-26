3a4034074c4205c8743ec0d977303cdfb5c27e59
Daniel Plata <danielplata79@gmail.com>
2024-05-25 21:08:18 -0500

    added z axis

--- worm.js
+++ worm.js
@@ -2,15 +2,13 @@ var inc = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
-    widoth = windowWidth;
+    width = windowWidth;
 
     angleMode(DEGREES);
 }
 
 function draw() {
-    background(0);
-
-    rotate(60);
+    background("lightcyan");
 
     var r = map(cos(frameCount/2), -1,1, 100, 200);
     var g = map(sin(frameCount/2), 0,1,100,200);
@@ -18,14 +16,15 @@ function draw() {
     stroke(r,g,b);
     noFill();
 
-    for(var i = 0; i < 20; i++) {
+
+    for(var i = 0; i < 40; i++) {
         beginShape();
         for(var j = 0; j < 360; j+= 10) {
             let r = i * 8;
             let x = r * cos(j);
             let y = r * sin(j);
 
-            vertex(x,y);
+            vertex(x,y, z);
         }
         endShape(CLOSE);
     }

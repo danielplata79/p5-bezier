12e6f5707762f050df48e26a3fa7e4d52d6ad251
Daniel Plata <danielplata79@gmail.com>
2024-05-25 20:35:54 -0500

    created loop for full simple radius circle in 3D

--- worm.js
+++ worm.js
@@ -1,7 +1,20 @@
-function setup() {
+var inc = 0;
 
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    widoth = windowWidth;
 }
 
 function draw() {
+    background(0);
 
+    rotate(60);
+
+    for(var i = 0; i < 20; i++) {
+        beginShape();
+        for(var j = 0; j < 360, j+= 20) {
+            
+        }
+        endShape();
+    }
 }
